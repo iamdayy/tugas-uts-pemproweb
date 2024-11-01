30021c1112c56b617706343360738a47f9d31178
iamdayy <108299143+iamdayy@users.noreply.github.com>
2024-11-01 14:30:00 +0700

    initial

---
+++ search.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name = "format-detection" content = "telephone=no" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- font-awesome font -->
+    <link rel="stylesheet" href="fonts/font-awesome.css" type="text/css" media="screen">
+    <!-- font-awesome font end -->
+    
+	<script src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header id="header" class="header2">
+  <div class="container">
+      <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="Weddings" src="img/logo.png"></a></h1>
+  </div>
+  <div class="menuheader">
+  <div class="bgmenu"></div>
+    <div class="container">
+    <div class="clearfix">
+        <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+            <ul class="nav sf-menu">
+              <li><a href="index.html">home</a>
+                <ul>
+                  <li><a href="index-1.html">About us</a></li>
+                  <li><a href="#">news</a></li>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="index-2.html">Portfolio</a></li>
+                  <li><a href="#">events</a></li>
+                  <li><a href="#">Testimonials</a></li>
+                  <li><a href="index-4.html">locations</a></li>
+                </ul>
+              </li>
+              <li><a href="index-1.html">about</a></li>
+              <li><a href="index-2.html">what we do</a></li>
+              <li><a href="index-3.html">blog</a></li>
+              <li><a href="index-4.html">contact</a></li>
+            </ul>
+        </nav>
+        </div>
+      </div>
+  </div>
+</header>
+<!--==============================content=================================-->
+<section id="content">
+    <!--==============================row10=================================-->
+    <div class="row_10">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+           <h2 class="h2-pad">Search result:</h2>
+            <div id="search-results"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-push-7 col-md-push-6 col-sm-push-6 col-lg-5 col-md-5 col-sm-6 colfooter">
+                <div class="row">
+                    <div class="col-lg-5 col-md-5 col-sm-5 colfooter2">                       
+                        <ul class="list2">
+                            <li>
+                               <a href="#">Twitter</a>
+                            </li>
+                            <li>
+                               <a href="#">Dribbble</a>
+                            </li>
+                            <li>
+                               <a href="#">Facebook</a>
+                            </li>
+                            <li>
+                               <a href="#">Behance</a>
+                            </li>
+                            <li>
+                               <a href="#">Skype</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6 col-md-7 col-sm-7 colfooter2">
+                        <p class="title5">8901 Marmora Road,<br>Glasgow, D04 89GR.<br>Freephone: +1 800 559 6580</p>
+                        <a class="btn-link btn-link1" href="#">www.demolink.org</a><br>
+                        <a class="btn-link btn-link1" href="#">mail@demolink.org</a>
+                    </div>
+                </div>   
+            </div>
+            <div class="col-lg-7 col-md-7 col-sm-6 col-lg-pull-5 col-md-pull-5 col-sm-pull-6 colfooter">
+                <a class="smalllogo" href="index.html"><img alt="Weddings" src="img/logosmall.png"></a>  
+                <p class="privacytxt"><span>&copy; 2013 &bull; </span><a class="footerprivlink" href="index-5.html"> Privacy policy</a></p> 
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
